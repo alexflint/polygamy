b89b9f9ebbf5d0f3cb5a2829b6662e4e51e0cdaf
alexflint <alex.flint@gmail.com>
2014-07-20 18:58:53 -0400

    add first and second derivatives of bezier curve with unittests

--- spline.py
+++ spline.py
@@ -1,7 +1,31 @@
-__author__ = 'Alex Flint'
+import numpy as np
+
 
 def evaluate_bezier(params, t):
+    """Evaluate a bezier curve at time t (between 0 and 1)"""
     if len(params) == 1:
         return params[0]
     else:
-        return evaluate_bezier(params[:-1], t) * (1-t) + evaluate_bezier(params[1:], t) * t
+        return evaluate_bezier(params[:-1], t) * (1.-t) + evaluate_bezier(params[1:], t) * t
+
+
+def evaluate_bezier_deriv(params, t):
+    if len(params) == 1:
+        return 0.
+    else:
+        a = evaluate_bezier(params[:-1], t)
+        b = evaluate_bezier(params[1:], t)
+        aderiv = evaluate_bezier_deriv(params[:-1], t)
+        bderiv = evaluate_bezier_deriv(params[1:], t)
+        return aderiv*(1.-t) + bderiv*t - a + b
+
+
+def evaluate_bezier_second_deriv(params, t):
+    if len(params) == 1:
+        return 0.
+    else:
+        aderiv = evaluate_bezier_deriv(params[:-1], t)
+        bderiv = evaluate_bezier_deriv(params[1:], t)
+        aderiv2 = evaluate_bezier_second_deriv(params[:-1], t)
+        bderiv2 = evaluate_bezier_second_deriv(params[1:], t)
+        return aderiv2*(1.-t) + bderiv2*t - aderiv*2. + bderiv*2.
