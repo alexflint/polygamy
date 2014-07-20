29e1ca81a18e400be68ca7bc2b99ff63922bbbc2
alexflint <alex.flint@gmail.com>
2014-07-20 18:18:16 -0400

    Factor out misc utils

---
+++ scripts/utils.py
@@ -0,0 +1,92 @@
+import StringIO
+from fractions import Fraction
+import numpy as np
+
+from polynomial import Polynomial, polynomial_jacobian
+
+
+def skew(m):
+    m = np.asarray(m)
+    return np.array([[ 0.,   -m[2],  m[1]],
+                     [ m[2],  0.,   -m[0]],
+                     [-m[1],  m[0],  0.  ]])
+
+
+def is_squarefree(fs, zeros):
+    J = polynomial_jacobian(fs)
+    for zero in zeros:
+        Jcur = J(*zero)
+        print Jcur
+
+
+def array_str(arr):
+    strings = []
+    maxlen = 0
+    for row in arr:
+        rowstrings = []
+        for elem in row:
+            if isinstance(elem, Polynomial):
+                s = elem.format(compact=True)
+            else:
+                s = unicode(elem)
+            rowstrings.append(s)
+            maxlen = max(maxlen, len(s))
+        strings.append(rowstrings)
+
+    ss = StringIO.StringIO()
+    ss.write('[')
+    for i,rowstrings in enumerate(strings):
+        if i > 0:
+            ss.write('\n ')
+        ss.write('[')
+        for j,s in enumerate(rowstrings):
+            if j > 0:
+                ss.write('  ')
+            ss.write(s)
+            ss.write(' '*(maxlen-len(s)))
+        ss.write(']')
+    ss.write(']')
+    return ss.getvalue()
+
+
+def arraymap(f, x):
+    if isinstance(x, Polynomial):
+         return f(x)
+    else:
+        try:
+            return np.array(map(lambda xi: arraymap(f, xi), x))
+        except:
+            return f(x)
+
+
+def astype(x, ctype):
+    return arraymap(lambda xi: xi.astype(ctype) if isinstance(xi, Polynomial) else ctype(xi), x)
+
+
+def asfraction(x, denom_limit=None):
+    return arraymap(lambda xi: Fraction(xi).limit_denominator(denom_limit), x)
+
+
+def flatten(x):
+    L = []
+    for xi in x:
+        if isinstance(xi, Polynomial):
+            L.append(xi)
+        else:
+            try:
+                L.extend(xi)
+            except:
+                L.append(xi)
+    return L
+
+
+def cayley_mat(s):
+    return np.eye(3) * (1. - np.dot(s,s)) + 2.*skew(s) + 2.*np.outer(s,s)
+
+
+def cayley_denom(s):
+    return 1. + np.dot(s,s)
+
+
+def cayley(s):
+    return cayley_mat(s) / cayley_denom(s)
