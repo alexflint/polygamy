31287a74df04295cd2b8f37371d4bf487c0faa87
alexflint <alex.flint@gmail.com>
2014-07-26 18:42:57 -0400

    Woah we can incorporate epipolar constraints from *all* pairs of frames and the problem is still linear!

--- scripts/run_relaxed_ba.py
+++ scripts/run_relaxed_ba.py
@@ -5,7 +5,7 @@ import numpy as np
 import numpy.linalg
 import scipy.optimize
 
-from polynomial import Polynomial, parse, matrix_form
+from polynomial import Polynomial, matrix_form, quadratic_form
 from polynomial_io import load_polynomials, load_functions,load_solution, write_solution, write_polynomials
 from spline import evaluate_zero_offset_bezier, evaluate_zero_offset_bezier_second_deriv
 from utils import cayley, cayley_mat, cayley_denom, skew, evaluate_array
@@ -22,10 +22,10 @@ def normalized(x):
 def essential_matrix(R1, p1, R2, p2):
     Rrel = np.dot(R2, R1.T)
     prel = np.dot(R1, p2-p1)
-    return essential_matrix_from_relative_pos(Rrel, prel)
+    return essential_matrix_from_relative_pose(Rrel, prel)
 
 
-def essential_matrix_from_relative_pos(Rrel, prel):
+def essential_matrix_from_relative_pose(Rrel, prel):
     return np.dot(Rrel, skew(prel))
 
 
@@ -333,9 +333,9 @@ def run_position_only_spline_epipolar():
     #
     # Construct ground truth
     #
-    num_landmarks = 10
-    num_frames = 3
-    num_imu_readings = 8
+    num_landmarks = 50
+    num_frames = 4
+    num_imu_readings = 80
     bezier_degree = 4
     out = 'out/position_only_bezier3'
 
@@ -363,7 +363,7 @@ def run_position_only_spline_epipolar():
     true_imu_cayleys = np.array([evaluate_zero_offset_bezier(true_rot_controls, t) for t in imu_times])
     true_imu_rotations = map(cayley, true_imu_cayleys)
 
-    true_gravity = np.zeros(3)  # np.random.rand(3) * 9.8
+    true_gravity = normalized(np.random.rand(3)) * 9.8
     true_accel_bias = np.random.rand(3)
     true_global_accels = np.array([evaluate_zero_offset_bezier_second_deriv(true_pos_controls, t) for t in imu_times])
     true_accels = [np.dot(R, a + true_gravity) + true_accel_bias
@@ -379,13 +379,16 @@ def run_position_only_spline_epipolar():
     #
     position_offs = 0
     accel_bias_offset = position_offs + (bezier_degree-1)*3
-    num_vars = accel_bias_offset + 3
+    gravity_offset = accel_bias_offset + 3
+    num_vars = gravity_offset + 3
 
     sym_vars = [Polynomial.coordinate(i, num_vars, Fraction) for i in range(num_vars)]
     sym_pos_controls = np.reshape(sym_vars[position_offs:position_offs+(bezier_degree-1)*3], (bezier_degree-1, 3))
     sym_accel_bias = np.asarray(sym_vars[accel_bias_offset:accel_bias_offset+3])
+    sym_gravity = np.asarray(sym_vars[gravity_offset:gravity_offset+3])
 
-    true_vars = np.hstack((true_pos_controls.flatten(), true_accel_bias))
+    true_vars = np.hstack((true_pos_controls.flatten(), true_accel_bias, true_gravity))
+    assert len(true_vars) == len(sym_vars)
 
     residuals = []
 
@@ -396,7 +399,7 @@ def run_position_only_spline_epipolar():
     for i in range(num_imu_readings):
         true_R = true_imu_rotations[i]
         sym_global_accel = evaluate_zero_offset_bezier_second_deriv(sym_pos_controls, imu_times[i])
-        sym_accel = np.dot(true_R, sym_global_accel) + sym_accel_bias
+        sym_accel = np.dot(true_R, sym_global_accel + sym_gravity) + sym_accel_bias
         residual = sym_accel - true_accels[i]
         for i in range(3):
             print '  Degree of global accel = %d, local accel = %d, residual = %d' % \
@@ -457,20 +460,27 @@ def run_position_only_spline_epipolar():
     null_space_dims = sum(np.abs(S) < 1e-5)
     print '\nNull space dimensions:', null_space_dims
     if null_space_dims > 0:
-        for i in null_space_dims:
+        for i in range(null_space_dims):
             print '  ',V[-i]
 
-    coordinate_monomials = [list(var.monomials)[0] for var in sym_vars]
     null_monomial = (0,) * num_vars
+    coordinate_monomials = [list(var.monomials)[0] for var in sym_vars]
     A, _ = matrix_form(gradients, coordinate_monomials)
     b, _ = matrix_form(gradients, [null_monomial])
-    x = np.squeeze(numpy.linalg.solve(A, -b))
+    b = np.squeeze(b)
+
+    AA, bb, kk = quadratic_form(cost)
+
+    estimated_vars = np.squeeze(numpy.linalg.solve(AA*2, -b))
 
     print '\nEstimated:'
-    print x
+    print estimated_vars
 
     print '\nGround truth:'
     print true_vars
+
+    print '\nError:'
+    print np.linalg.norm(estimated_vars - true_vars)
 
     # Output to file
     write_polynomials(cost, out+'/cost.txt')
@@ -478,6 +488,174 @@ def run_position_only_spline_epipolar():
     write_polynomials(gradients, out+'/gradients.txt')
     write_polynomials(jacobians.flat, out+'/jacobians.txt')
     write_solution(true_vars, out+'/solution.txt')
+
+
+def run_position_only_spline_epipolar_simplified():
+    #
+    # Construct ground truth
+    #
+    num_landmarks = 50
+    num_frames = 4
+    num_imu_readings = 80
+    bezier_degree = 4
+    out = 'out/position_only_bezier3'
+
+    print 'Num landmarks:', num_landmarks
+    print 'Num frames:', num_frames
+    print 'Num IMU readings:', num_imu_readings
+    print 'Bezier curve degree:', bezier_degree
+
+    if not os.path.isdir(out):
+        os.mkdir(out)
+
+    # Both splines should start at 0,0,0
+    frame_times = np.linspace(0, .9, num_frames)
+    imu_times = np.linspace(0, 1, num_imu_readings)
+
+    true_rot_controls = np.random.rand(bezier_degree-1, 3)
+    true_pos_controls = np.random.rand(bezier_degree-1, 3)
+
+    true_landmarks = np.random.randn(num_landmarks, 3)
+
+    true_frame_cayleys = np.array([evaluate_zero_offset_bezier(true_rot_controls, t) for t in frame_times])
+    true_frame_orientations = np.array(map(cayley, true_frame_cayleys))
+    true_frame_positions = np.array([evaluate_zero_offset_bezier(true_pos_controls, t) for t in frame_times])
+
+    true_imu_cayleys = np.array([evaluate_zero_offset_bezier(true_rot_controls, t) for t in imu_times])
+    true_imu_orientations = np.array(map(cayley, true_imu_cayleys))
+
+    true_gravity = normalized(np.random.rand(3)) * 9.8
+    true_accel_bias = np.random.rand(3)
+    true_global_accels = np.array([evaluate_zero_offset_bezier_second_deriv(true_pos_controls, t) for t in imu_times])
+    true_accels = np.array([np.dot(R, a + true_gravity) + true_accel_bias
+                            for R, a in zip(true_imu_orientations, true_global_accels)])
+
+    true_features = np.array([[normalized(np.dot(R, x-p)) for x in true_landmarks]
+                              for R, p in zip(true_frame_orientations, true_frame_positions)])
+
+    print np.min(true_features.reshape((-1, 3)), axis=0)
+    print np.max(true_features.reshape((-1, 3)), axis=0)
+
+    #
+    # Add sensor noise
+    #
+
+    accel_noise = 0
+    feature_noise = .001
+
+    observed_frame_orientations = true_frame_orientations.copy()
+    observed_imu_orientations = true_imu_orientations.copy()
+    observed_features = true_features.copy()
+    observed_accels = true_accels.copy()
+
+    if accel_noise > 0:
+        observed_accels += np.random.randn(*observed_accels.shape) * accel_noise
+
+    if feature_noise > 0:
+        observed_features += np.random.rand(*observed_features.shape) * feature_noise
+
+    #
+    # Construct symbolic versions of the above
+    #
+    position_offs = 0
+    accel_bias_offset = position_offs + (bezier_degree-1)*3
+    gravity_offset = accel_bias_offset + 3
+    num_vars = gravity_offset + 3
+
+    sym_vars = [Polynomial.coordinate(i, num_vars, Fraction) for i in range(num_vars)]
+    sym_pos_controls = np.reshape(sym_vars[position_offs:position_offs+(bezier_degree-1)*3], (bezier_degree-1, 3))
+    sym_accel_bias = np.asarray(sym_vars[accel_bias_offset:accel_bias_offset+3])
+    sym_gravity = np.asarray(sym_vars[gravity_offset:gravity_offset+3])
+
+    true_vars = np.hstack((true_pos_controls.flatten(), true_accel_bias, true_gravity))
+    assert len(true_vars) == len(sym_vars)
+
+    #
+    # Accel residuals
+    #
+    residuals = []
+
+    print '\nAccel residuals:'
+    for i in range(num_imu_readings):
+        observed_R = observed_imu_orientations[i]
+        sym_global_accel = evaluate_zero_offset_bezier_second_deriv(sym_pos_controls, imu_times[i])
+        sym_accel = np.dot(observed_R, sym_global_accel + sym_gravity) + sym_accel_bias
+        residual = sym_accel - observed_accels[i]
+        residuals.extend(residual)
+
+    #
+    # Epipolar residuals
+    #
+    #for i in range(1, num_frames):
+    #    observed_R = observed_frame_orientations[i]
+    #    sym_p = evaluate_zero_offset_bezier(sym_pos_controls, frame_times[i])
+    #    sym_E = essential_matrix_from_relative_pose(observed_R, sym_p)
+    #    for j in range(num_landmarks):
+    #        z = observed_features[i][j]
+    #        z0 = observed_features[0][j]
+    #        residual = np.dot(z, np.dot(sym_E, z0))
+    #        residuals.append(residual)
+
+    for i in range(num_frames):
+        observed_Ri = observed_frame_orientations[i]
+        sym_pi = evaluate_zero_offset_bezier(sym_pos_controls, frame_times[i])
+        for j in range(num_frames):
+            observed_Rj = observed_frame_orientations[j]
+            sym_pj = evaluate_zero_offset_bezier(sym_pos_controls, frame_times[j])
+            if i == j: continue
+            sym_E = essential_matrix(observed_Ri, sym_pi, observed_Rj, sym_pj)
+            for k in range(num_landmarks):
+                zi = observed_features[i][k]
+                zj = observed_features[j][k]
+                residual = np.dot(zj, np.dot(sym_E, zi))
+                residuals.append(residual)
+
+    print '\nNum vars:', num_vars
+    print 'Num residuals:', len(residuals)
+
+    print '\nResiduals:', len(residuals)
+    cost = Polynomial(num_vars)
+    for r in residuals:
+        cost += r*r
+        print '  %f   (degree=%d, length=%d)' % (r(*true_vars), r.total_degree, len(r))
+
+    print '\nCost:'
+    print '  Num terms: %d' % len(cost)
+    print '  Degree: %d' % cost.total_degree
+
+    # Solve
+    A, b, k = quadratic_form(cost)
+    estimated_vars = np.squeeze(numpy.linalg.solve(A*2, -b))
+    estimated_pos_controls = np.reshape(estimated_vars[position_offs:position_offs+(bezier_degree-1)*3], (bezier_degree-1, 3))
+    estimated_accel_bias = np.asarray(estimated_vars[accel_bias_offset:accel_bias_offset+3])
+    estimated_gravity = np.asarray(estimated_vars[gravity_offset:gravity_offset+3])
+    estimated_positions = np.array([evaluate_zero_offset_bezier(estimated_pos_controls, t) for t in frame_times])
+
+    print '\nEstimated:'
+    print estimated_vars
+
+    print '\nGround truth:'
+    print true_vars
+
+    print '\nTotal Error:', np.linalg.norm(estimated_vars - true_vars)
+    print 'Accel bias error:', np.linalg.norm(estimated_accel_bias - true_accel_bias)
+    print 'Gravity error:', np.linalg.norm(estimated_gravity - true_gravity)
+    for i in range(num_frames):
+        print '  Frame %d error: %f' % (i, np.linalg.norm(estimated_positions[i] - true_frame_positions[i]))
+
+    fig = plt.figure(figsize=(14,6))
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+
+    ts = np.linspace(0, 1, 100)
+    true_ps = np.array([evaluate_zero_offset_bezier(true_pos_controls, t) for t in ts])
+    opt_ps = np.array([evaluate_zero_offset_bezier(estimated_pos_controls, t) for t in ts])
+
+    ax.plot(true_ps[:,0], true_ps[:,1], true_ps[:,2], '-b')
+    ax.plot(opt_ps[:,0], opt_ps[:,1], opt_ps[:,2], '-r')
+
+    #ax.plot(true_landmarks[:,0], true_landmarks[:,1], true_landmarks[:,2], '.r')
+
+    plt.show()
 
 
 def analyze_polynomial():
@@ -564,7 +742,7 @@ def analyze_polynomial2():
     #
     # TODO:
     #  - [done] add accel bias
-    #  - add gravity
+    #  - [done] add gravity
     #  - [ignore] add gyro bias and gyro measurements
     #    - may need to solve for gyro bias separately
     #  - investigate noise
@@ -581,7 +759,8 @@ def main():
     #run_sfm()
     #run_epipolar()
     #run_spline_epipolar()
-    run_position_only_spline_epipolar()
+    #run_position_only_spline_epipolar()
+    run_position_only_spline_epipolar_simplified()
 
     #analyze_polynomial()
     #analyze_polynomial2()
