8be13403207bf9a3c9ded98ca92c63a91d189717
alexflint <alex.flint@gmail.com>
2014-08-03 19:49:29 -0400

    Change spline to use coefficients

--- spline.py
+++ spline.py
@@ -1,43 +1,49 @@
 import numpy as np
 
 
-def evaluate_bezier(params, t):
+def bezier(params, t):
     """Evaluate a bezier curve at time t (between 0 and 1)"""
-    if len(params) == 1:
-        return params[0]
+    return np.dot(bezier_coefs(t, len(params)-1), params)
+
+
+def bezier_coefs(t, order):
+    """Evaluate a bezier curve at time t (between 0 and 1)"""
+    if order == 0:
+        return np.array([1.])
     else:
-        return evaluate_bezier(params[:-1], t) * (1.-t) + evaluate_bezier(params[1:], t) * t
+        c = bezier_coefs(t, order-1)
+        return np.hstack((c, 0)) * (1.-t) + np.hstack((0, c)) * t
 
 
-def evaluate_bezier_deriv(params, t):
+def bezier_deriv(params, t):
     if len(params) == 1:
         return 0.
     else:
-        a = evaluate_bezier(params[:-1], t)
-        b = evaluate_bezier(params[1:], t)
-        aderiv = evaluate_bezier_deriv(params[:-1], t)
-        bderiv = evaluate_bezier_deriv(params[1:], t)
+        a = bezier(params[:-1], t)
+        b = bezier(params[1:], t)
+        aderiv = bezier_deriv(params[:-1], t)
+        bderiv = bezier_deriv(params[1:], t)
         return aderiv*(1.-t) + bderiv*t - a + b
 
 
-def evaluate_bezier_second_deriv(params, t):
+def bezier_second_deriv(params, t):
     if len(params) == 1:
         return 0.
     else:
-        aderiv = evaluate_bezier_deriv(params[:-1], t)
-        bderiv = evaluate_bezier_deriv(params[1:], t)
-        aderiv2 = evaluate_bezier_second_deriv(params[:-1], t)
-        bderiv2 = evaluate_bezier_second_deriv(params[1:], t)
+        aderiv = bezier_deriv(params[:-1], t)
+        bderiv = bezier_deriv(params[1:], t)
+        aderiv2 = bezier_second_deriv(params[:-1], t)
+        bderiv2 = bezier_second_deriv(params[1:], t)
         return aderiv2*(1.-t) + bderiv2*t - aderiv*2. + bderiv*2.
 
 
-def evaluate_zero_offset_bezier(params, t):
+def zero_offset_bezier(params, t):
     if t == 0:
         # should return a numpy array for t=0, not a Polynomial
         return np.zeros_like(params[0])
     else:
-        return evaluate_bezier(np.vstack((np.zeros(len(params[0])), params)), t)
+        return bezier(np.vstack((np.zeros(len(params[0])), params)), t)
 
 
-def evaluate_zero_offset_bezier_second_deriv(params, t):
-    return evaluate_bezier_second_deriv(np.vstack((np.zeros(len(params[0])), params)), t)
+def zero_offset_bezier_second_deriv(params, t):
+    return bezier_second_deriv(np.vstack((np.zeros(len(params[0])), params)), t)
