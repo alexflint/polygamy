Fixed spline derivatives

spline.py
@@ -21,7 +21,7 @@ def bezier_deriv(params, t):
 
 def bezier_deriv_coefs(t, order):
     if order == 0:
-        return np.array([1.])
+        return np.array([0.])
     else:
         c = bezier_coefs(t, order-1)
         dc = bezier_deriv_coefs(t, order-1)
@@ -34,7 +34,7 @@ def bezier_second_deriv(params, t):
 
 def bezier_second_deriv_coefs(t, order):
     if order == 0:
-        return np.array([1.])
+        return np.array([0.])
     else:
         dc = bezier_deriv_coefs(t, order-1)
         ddc = bezier_second_deriv_coefs(t, order-1)
@@ -42,8 +42,7 @@ def bezier_second_deriv_coefs(t, order):
 
 
 def repeat_diag(x, k):
-    x = np.asarray(x)
-    return np.kron(np.eye(k, dtype=x.dtype), x)
+    return np.hstack([np.eye(k, dtype=x.dtype) * xi for xi in x])
 
 
 def zero_offset_bezier(params, t):

test_spline.py
@@ -5,6 +5,7 @@ import numpy as np
 
 import spline
 
+
 class BezierTest(unittest.TestCase):
     def test_derivative(self):
         w = np.array([1.7, 2.8, 1.4, -3.6])
@@ -28,7 +29,7 @@ class BezierTest(unittest.TestCase):
         np.testing.assert_array_almost_equal(g2(.9),
                                              spline.bezier_second_deriv(w, .9))
 
-    def test_derivative_multidimensional(self):
+    def test_second_derivative_multidimensional(self):
         np.random.seed(123)
         w = np.random.rand(4, 3)
         f = lambda t: spline.bezier(w, t)
@@ -40,3 +41,29 @@ class BezierTest(unittest.TestCase):
         np.testing.assert_array_almost_equal(g2,
                                              spline.bezier_second_deriv(w, .9))
 
+    def test_zero_offset_second_derivative(self):
+        w = np.array([1.7, 2.8, 1.4, -3.6])
+        f = lambda t: spline.zero_offset_bezier(w, t)
+        g2 = numdifftools.Hessdiag(f)
+        np.testing.assert_array_almost_equal(g2(.9),
+                                             spline.zero_offset_bezier_second_deriv(w, .9))
+
+    def test_zero_offset_bezier_mat(self):
+        bezier_order = 4
+        t = .5
+        f = lambda x: spline.zero_offset_bezier(x.reshape((bezier_order, 3)), t)
+
+        J_numeric = numdifftools.Jacobian(f)(np.zeros(bezier_order*3))
+        J_analytic = spline.zero_offset_bezier_mat(t, bezier_order, 3)
+
+        np.testing.assert_array_almost_equal(J_numeric, J_analytic)
+
+    def test_zero_offset_bezier_second_deriv_mat(self):
+        bezier_order = 4
+        t = .5
+        f = lambda x: spline.zero_offset_bezier_second_deriv(x.reshape((bezier_order, 3)), t)
+
+        J_numeric = numdifftools.Jacobian(f)(np.zeros(bezier_order*3))
+        J_analytic = spline.zero_offset_bezier_second_deriv_mat(t, bezier_order, 3)
+
+        np.testing.assert_array_almost_equal(J_numeric, J_analytic)